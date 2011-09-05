52c06ebe4f3b070f289e7735ebef65b608852fda
tisa007 <wp.tisa@gmail.com>
2011-09-05 09:32:10 +0800

    init by rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charmbuy::Application.config.secret_token = '383e9295f5da35e658157900af689abefabd647f3dbedcd7ec57b84f47d9eefa55d9a4937398c7fecc9eaa9283ee42d2f584419e0c75a6a6212fac6926fce227'
